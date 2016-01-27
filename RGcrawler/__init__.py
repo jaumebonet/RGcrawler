0bd0f9627a702abf64574f5c079bab5c723f1fcb
Jaume Bonet <jaume.bonet@gmail.com>
2016-01-27 17:17:03 +0100

    setup.py bugfix

--- RGcrawler/__init__.py
+++ RGcrawler/__init__.py
@@ -1,8 +1,7 @@
+__version__ = '0.0.1'
 
 from .Contribution      import Contribution
 from .Author            import Author
 from .ScientificSociety import ScientificSociety
-
-__version__ = '0.0.1'
 
 __all__ = [ "ScientificSociety", "Contribution", "Author"]
